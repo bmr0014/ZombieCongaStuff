1ee5a457e92ad057e3a98181c8ab95d58921b493
Bree Roman <bmr0014@csse-imac8.cse.eng.auburn.edu>
2016-09-15 12:34:05 -0500

    Initial commit yo

---
+++ Zombie Congo/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  Zombie Congo
+//
+//  Created by Bree Roman on 9/15/16.
+//  Copyright (c) 2016 Bree Roman. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+    backgroundColor = SKColor.blueColor()
+    }
+}
